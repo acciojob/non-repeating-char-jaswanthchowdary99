45ca3ea43f398df7452a76064d71eb03347214ee
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-08 11:16:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = {};
+	str.split('').forEach((char)=>{
+		count[char] = (count[char] || 0) + 1 ;
+	})
+	let nonRepeatedChar =  str.split('').find(char => count[char] == 1);
+	return nonRepeatedChar
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
